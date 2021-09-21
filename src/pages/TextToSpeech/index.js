3e62c200d44ad2d07b10b5f4b3bf247df9972111
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-09-21 09:48:49 -0300

    ajuste responsividade

--- src/pages/TextToSpeech/index.js
+++ src/pages/TextToSpeech/index.js
@@ -2,10 +2,11 @@ import ProjectCard from "../../components/ProjectCard";
 import Header from "../../components/Header"
 import watson from "../../imagens/textToSpeech.gif";
 import aboutWatson from "./aboutWatson";
+import './TextToSpeech.css';
 
 function textToSpeech() {
   return (
-    <div className="Projects">
+    <div className="TextToSpeech">
       <Header/>
       <section className="Projects-section">
       <ProjectCard gif={watson} info={ aboutWatson } />
